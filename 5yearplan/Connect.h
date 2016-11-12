a1b5b8262327f6666655da38bb4b9c2435367a0c
Eva <eva.yw.yu@gmail.com>
2016-11-12 09:44:11 -0800

    EY:  Added Connect.h Header file for IDLE state and Controlling Rx and Tx

---
+++ 5yearplan/Connect.h
@@ -0,0 +1,59 @@
+/*------------------------------------------------------------------------------
+-- SOURCE FILE: Command.cpp - The COMMAND or IDLE state of the protocol
+--
+-- PROGRAM: [5YearPlan] 
+--
+-- FUNCTIONS:
+-- *list all functions here!* 
+-- [returnType] [funcName] (funcParams)
+--
+-- DATE: Nov. 09, 2016
+--
+-- REVISIONS: 
+-- Version 1.2.
+--
+-- DESIGNER: Eva Yu
+--
+-- PROGRAMMER: Eva Yu
+--
+-- NOTES:
+-- This is the main starting point for the communication once the port has been open.  
+------------------------------------------------------------------------------*/
+#pragma once
+
+#include <thread>
+#include <Win32>
+#include "timer.h"
+
+namespace Connect{
+
+   //Default timer with member function pointer
+    class Connection {
+    private: 
+        static const int IDLE_TIMER      = 500 ; // in ms
+        static const int RAN_TIMER_START = 0 ; // in ms
+        static const int RAN_TIMER_STOP  = 100 ; // in ms
+
+        std::thread readThread;
+        std::thread writeThread;
+        Timer <Connection, & startTx, RAN_TIMER_START, RAN_TIMER_STOP> randomEnqTimer;
+        Timer <Connection, & startRandomEnqTimer , IDLE_STATE_TIME > idleStateTimer;
+        
+        Transmit::Transmitter   TX;
+        /****REPLACE WITH RX OBJECT!!!****/
+        Recieve::Reciever       RX;
+
+        void readInBuffer(); // event driven
+        void enqLine();
+        void startTx(); 
+        void startRx();
+        void startRandomEnqTimer();
+        void startIdleStateTimer();
+
+    public:
+        Connect();
+        bool startConnnection();
+        bool stopConnnection();
+    };
+
+}
