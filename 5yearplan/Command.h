050f96441eb0f344dbac3868a6d026136d490f69
megapers <megapers@gmail.com>
2016-11-12 16:18:31 -0800

    Adding Command files to REPO

---
+++ 5yearplan/Command.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include "winmenu2.h"
+
+char Name[] = "Wireless communication portal";
+LPCSTR	lpszCommName = "COM5";
+HANDLE hComm;
+char str[80] = "";
+bool connected = false;
+OPENFILENAME ofn;// Structure that contains attachment file info
+char szFile[100];// Memory buffer to contain the file name
+
+//Function prototypes
+void generateViews(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void selectCommPort(HANDLE, LPCSTR, HWND, bool&);
+void connect(HANDLE&, LPCSTR, bool&);
+void disconnect(HANDLE&, bool&, LPCSTR);
+void createUIWindows(HWND);
+void attach(void);
+
+//Window handlers and variables
+static const int WIN_LENGTH = 500;		  // Main window length
+static const int WIN_WIDTH = 650;		  // Main window height
+
+//DATA MEMBERS
+static const DWORD BUFF_SIZE = 256;
+static const DWORD TEXTBOX_HEIGTH = 200;
+static const DWORD TEXTBOX_WIDTH = 400;
+static const DWORD USERINPUT_TEXTBOX_START_X = 190;
+static const DWORD USERINPUT_TEXTBOX_START_Y = 10;
+static const DWORD READINPUT_TEXTBOX_START_X = 190;
+static const DWORD READINPUT_TEXTBOX_START_Y = 220;
+static const DWORD BUTTON_WIDTH = 100;
+static const DWORD BUTTON_HEIGHT = 30;
+static const DWORD DISCONNECT_BUTTON_X = 50;
+static const DWORD DISCONNECT_BUTTON_Y = 150;
+static const DWORD CONNECT_BUTTON_X = 50;
+static const DWORD CONNECT_BUTTON_Y = 250;
+
+HDC hdc;
+TEXTMETRIC tm;
+HWND userInputTextBox;
+HWND readInputTextBox;
+HWND hConnectButton;
+HWND hDisconnectButton;
+HWND browse;
---
+++ 5yearplan/main.cpp
@@ -0,0 +1,298 @@
+// Program WinMenu.cpp
+// COMP 3980, Final Project
+// Tim Makimov, A009031109
+
+#define STRICT
+
+#include "Command.h"
+
+#pragma warning (disable: 4096)
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
+	LPSTR lspszCmdParam, int nCmdShow)
+{
+	MSG Msg;
+	generateViews(hInst, nCmdShow);
+	while (GetMessage(&Msg, NULL, 0, 0))
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc = NULL;
+	PAINTSTRUCT paintstruct;
+
+	switch (Message)
+	{
+	case WM_COMMAND:
+
+		if ((HWND)lParam == hConnectButton)
+		{
+			connect(hComm, lpszCommName, connected);
+			break;
+		}
+		if ((HWND)lParam == hDisconnectButton)
+		{
+			disconnect(hComm, connected, lpszCommName);
+			break;
+		}
+		if ((HWND)lParam == browse)
+		{
+			attach();
+			break;
+		}
+		switch (LOWORD(wParam))
+		{
+		case IDM_COM1:
+			lpszCommName = "COM1";
+			selectCommPort(hComm, lpszCommName, hwnd, connected);
+			break;
+		case IDM_COM2:
+			lpszCommName = "COM2";
+			selectCommPort(hComm, lpszCommName, hwnd, connected);
+			break;
+		case IDM_COM3:
+			lpszCommName = "COM3";
+			selectCommPort(hComm, lpszCommName, hwnd, connected);
+			break;
+		case IDM_COM4:
+			lpszCommName = "COM4";
+			selectCommPort(hComm, lpszCommName, hwnd, connected);
+			break;
+		case IDM_HELP:
+			MessageBox(NULL, 
+					"Click CONNECT to connect to the default COMM port or select a COMM port from settings and then click CONNECT.", 
+					"", MB_OK);
+			break;
+		case IDM_Exit:
+			PostQuitMessage(0);
+			break;
+		}
+		break;
+
+	case WM_CHAR:	// Process keystroke
+		if (wParam == 27)
+		{
+			disconnect(hComm, connected, lpszCommName);
+		}
+		break;
+
+	case WM_PAINT:		// Process a repaint message
+		hdc = BeginPaint(hwnd, &paintstruct); // Acquire DC
+		TextOut(hdc, 0, 0, str, strlen(str)); // output character
+		EndPaint(hwnd, &paintstruct); // Release DC
+		break;
+
+	case WM_DESTROY:	// Terminate program
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+
+void generateViews(HINSTANCE hInst, int nCmdShow)
+{
+	HWND hwnd;
+	WNDCLASSEX Wcl;
+
+	Wcl.cbSize = sizeof(WNDCLASSEX);
+	Wcl.style = CS_HREDRAW | CS_VREDRAW;
+	Wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION); // large icon 
+	Wcl.hIconSm = NULL; // use small version of large icon
+	Wcl.hCursor = LoadCursor(NULL, IDC_ARROW);  // cursor style
+
+	Wcl.lpfnWndProc = WndProc;
+	Wcl.hInstance = hInst;
+	Wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //white background
+	Wcl.lpszClassName = Name;
+
+	Wcl.lpszMenuName = "MYMENU"; // The menu Class
+	Wcl.cbClsExtra = 0;      // no extra memory needed
+	Wcl.cbWndExtra = 0;
+
+	if (!RegisterClassEx(&Wcl))
+		return;
+
+	hwnd = CreateWindow(Name, 
+						Name, 
+						WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,			
+						CW_USEDEFAULT, CW_USEDEFAULT,
+						WIN_WIDTH, WIN_LENGTH,
+						NULL, //parent of window
+						NULL, //menu bar
+						hInst, // first aparam in WinMain
+						NULL);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	createUIWindows(hwnd);
+}
+
+void selectCommPort(HANDLE hComm, LPCSTR lpszCommName, HWND hwnd, bool &connected)
+{
+	COMMCONFIG	cc;
+	cc.dwSize = sizeof(COMMCONFIG);
+	cc.wVersion = 0x100;
+	GetCommConfig(hComm, &cc, &cc.dwSize);
+	if (!CommConfigDialog(lpszCommName, hwnd, &cc))
+	{
+		MessageBox(NULL, "Error connecting to COMM port", lpszCommName, MB_OK);
+		return;
+	}
+	else
+	{
+		//connect(hComm, lpszCommName, connected);
+	}
+	if ((SetCommState(hComm, &cc.dcb)) == FALSE)
+	{
+		return;
+	}
+}
+
+
+void connect(HANDLE &commPort, LPCSTR CommName, bool &connection)
+{
+	if (connection == false)
+	{
+		//if startConnection = false
+		if ((commPort = CreateFile(CommName, GENERIC_READ | GENERIC_WRITE, 0,
+			NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL))
+			== INVALID_HANDLE_VALUE)
+		{
+			MessageBox(NULL, "Error opening COM port", CommName, MB_OK);
+			//invoke HANDLE CONNECTION ERRORS
+		}
+		else
+		{
+			MessageBox(NULL, "Conected to port", CommName, MB_OK);
+			connection = true;
+			EnableWindow(hConnectButton, !connection);
+			EnableWindow(hDisconnectButton, connection);
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+
+void disconnect(HANDLE& commPort, bool& connection, LPCSTR CommName)
+{
+	if (connection == true)
+	{
+		CloseHandle(commPort);
+		//ExitThread(threadId);//terminate thread
+		MessageBox(NULL, "Disconnected from port", CommName, MB_OK);
+		connection = false;
+		EnableWindow(hDisconnectButton, connection);
+		EnableWindow(hConnectButton, !connection);
+	}
+	else
+	{
+		return;
+	}
+}
+
+
+void createUIWindows(HWND hwnd)
+{
+	userInputTextBox = CreateWindow("STATIC",
+		NULL,
+		WS_BORDER | WS_CHILD | WS_VISIBLE | WS_DISABLED,
+		USERINPUT_TEXTBOX_START_X,
+		USERINPUT_TEXTBOX_START_Y,
+		TEXTBOX_WIDTH,
+		TEXTBOX_HEIGTH,
+		hwnd,
+		NULL,
+		NULL,
+		NULL
+	);
+
+	readInputTextBox = CreateWindow(
+		"STATIC",
+		NULL,
+		WS_BORDER | WS_CHILD | WS_VISIBLE | WS_DISABLED, 
+		READINPUT_TEXTBOX_START_X,
+		READINPUT_TEXTBOX_START_Y,
+		TEXTBOX_WIDTH,
+		TEXTBOX_HEIGTH,
+		hwnd,
+		NULL,
+		NULL,
+		NULL
+	);
+	hConnectButton = CreateWindow(
+		"BUTTON",
+		"CONNECT",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		50,
+		200,
+		BUTTON_WIDTH,
+		BUTTON_HEIGHT,
+		hwnd,
+		NULL,
+		NULL,
+		NULL
+	);
+	hDisconnectButton = CreateWindow(
+		"BUTTON",
+		"DISCONNECT",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | WS_DISABLED,
+		50,
+		250,
+		BUTTON_WIDTH,
+		BUTTON_HEIGHT,
+		hwnd,
+		NULL,
+		NULL,
+		NULL
+	);
+
+	browse = CreateWindow(
+		"BUTTON",
+		"ATTACH",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		50,
+		300,
+		BUTTON_WIDTH,
+		BUTTON_HEIGHT,
+		hwnd,
+		NULL,
+		NULL,
+		NULL
+	);
+}
+
+void attach()
+{
+	// open a file name
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = NULL;
+	ofn.lpstrFile = szFile;
+	ofn.lpstrFile[0] = '\0';
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER | OFN_ENABLEHOOK;
+	
+	if (GetOpenFileName(&ofn))
+	{
+		// Now simpley display the file name 
+		MessageBox(NULL, ofn.lpstrFile, "File Name", MB_OK);
+	}
+
+	
+}
