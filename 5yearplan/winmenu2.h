35ef450411ef81861d8e4da546a1bdc3cd07a7cd
megapers <megapers@gmail.com>
2016-11-14 11:23:18 -0800

    TM: header file that holds macros

---
+++ 5yearplan/winmenu2.h
@@ -0,0 +1,26 @@
+#pragma once
+#define IDM_TX			100
+#define IDM_RX			101
+#define IDM_Port		102
+#define IDM_Speed		103
+#define IDM_Word		104
+#define IDM_ZETA		105
+#define IDM_Save		106
+#define IDM_THETA		107
+#define IDM_HELP		108
+#define IDM_NEWCONNECT	109
+#define IDM_PROPERTIES	110
+#define IDM_Settings		111
+#define IDM_COM4		112
+#define IDM_9600		113
+#define IDM_32000		114
+#define IDM_110000		115
+#define IDM_Connect		118
+#define IDM_Disconnect	119
+#define IDM_Exit		120
+#define IDM_COM_COMBOBOX	121
+#define IDM_OK			122
+#define IDM_CANCEL		123
+#define IDD_COMDIALOG	124
+
+
