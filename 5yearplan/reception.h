c7141164773e8177d0321ce9302b726462c05d3f
Terry <rkdxogns1234@daum.net>
2016-11-20 14:41:20 -0800

    added basic reception funtions

--- 5yearplan/reception.h
+++ 5yearplan/reception.h
@@ -11,33 +11,20 @@
 #include "packet.h"
 
 namespace receive {
-	class Reception {
-	private:
-		DWORD packetCounter;
-		DWORD errorCounter;
-		Process process;
-		BOOL isPacketTimedOut;
-
-		void sendACK(HANDLE handleCom);
-		BOOL waitForPacket(HANDLE handleCom, std::vector<BYTE> &buffer);
-		BOOL retrievePacket(HANDLE handleCom, std::vector<BYTE> &buffer);
-		BOOL parsePacket(Packet &packet, std::vector<BYTE> &buffer);
-		BOOL validatePacket(Packet &packet);
-		void errorStat(HWND hwnd);
-		void packetTimeout();
-		typedef Timer<Reception, &packetTimeout, (DATA_SIZE + CRC_SIZE + 1 / BAUD_RATE)*3> AckTimer;
-		AckTimer packetTimer;
-	public:
-		BOOL start(HWND handleWin, HANDLE handleCom);
-	};
+	static const DWORD TEXTBOX_HEIGTH = 200;
+	static const DWORD TEXTBOX_WIDTH = 400;
 
 	class Process {
 	private:
-		std::queue<BYTE *> dataQueue;
+		std::queue<std::string> dataQueue;
 		std::vector<char> writeBuffer;
 		BOOL isProcessing = false;
 		HANDLE processThread;
 		DWORD threadId;
+		HWND handleDisplay;
+		int char_x;
+		int char_y;
+
 		static DWORD WINAPI readCharacters(LPVOID params);
 		BOOL handleChar(char c);
 		void writeCharToBuffer(char c);
@@ -45,8 +32,32 @@ namespace receive {
 		BOOL saveBufferToFile();
 		void successSaveFile();
 		void failToSaveFile();
+		void cls();
 	public:
-		void startProcess(HWND &hwnd, BYTE *data);
+		void startProcess(HWND handleDisplayParam, std::string&);
 	};
+
+	class Reception {
+	private:
+		static void packetTimeout();
+		static BOOL isPacketTimedOut;
+
+		DWORD packetCounter;
+		DWORD errorCounter;
+		Process process;
+
+
+		void sendACK(HANDLE handleCom);
+		BOOL waitForPacket(HANDLE handleCom);
+		BOOL retrievePacket(HANDLE handleCom, std::vector<BYTE> &buffer);
+		BOOL parsePacket(Packet &packet, std::vector<BYTE> &buffer);
+		BOOL validatePacket(Packet &packet);
+		void errorStat(HWND handleStat);
+		typedef Timer<&packetTimeout, (PACKET_SIZE / BAUD_RATE) * 3000> PacketTimer;
+		PacketTimer packetTimer;
+	public:
+		BOOL start(HWND handleDisplay, HWND handleStat, HANDLE handleCom);
+	};
+
 };
 #endif // !RECEIVE_H
