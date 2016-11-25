f74ce01df64cc17cb591607d755bd6a03aa45583
JohnAgapeyev <jagapeyev@gmail.com>
2016-11-24 18:02:22 -0800

    Fixed issue where template specialization caused odd abort error at runtime

--- 5yearplan/timer.h
+++ 5yearplan/timer.h
@@ -10,16 +10,16 @@ Timer classes here take an unsigned long representing the number of milliseconds
 */
 
 //Random timer
-template<void (*ev)(), unsigned long minDuration, unsigned long maxDuration = 100>
+template<void (*ev)(), unsigned long long minDuration, unsigned long long maxDuration = 100>
 class Timer {
     std::thread thread;
     std::mutex mutex;
     std::condition_variable cv;
     bool active = false;
-    std::chrono::duration<unsigned long, std::milli> dur;
+    std::chrono::duration<unsigned long long, std::milli> dur;
 
     std::mt19937_64 randEng{std::random_device()()}; //Predefined 64 bit random engine using random device value as starting seed
-    std::uniform_int_distribution<unsigned long> uni{minDuration, maxDuration}; // guaranteed unbiased
+    std::uniform_int_distribution<unsigned long long> uni{minDuration, maxDuration}; // guaranteed unbiased
 
     unsigned long getRandDuration() {
         return uni(randEng);
@@ -58,7 +58,7 @@ public:
 
 //Default timer
 template<void(*ev)(), unsigned long duration>
-class Timer<ev, duration, 0> {
+class Timer<ev, duration, 100> {
     std::thread thread;
     std::mutex mutex;
     std::condition_variable cv;
