bd417cec2c62f72a3a0e6a54e34a62af90842e9f
megapers <megapers@gmail.com>
2016-11-24 16:48:12 -0800

    TM: Included Connect.h

--- 5yearplan/Command.h
+++ 5yearplan/Command.h
@@ -2,11 +2,13 @@
 #include <windows.h>
 #include <stdio.h>
 #include "winmenu2.h"
+#include "Connect.h"
+
 
 char Name[] = "Wireless communication portal";
 LPCSTR	lpszCommName = "COM5";
 char comPort[10];//Currently selected COMM PORT
-HANDLE hComm;
+//HANDLE hComm;
 char str[80] = "";
 bool connected = false;
 OPENFILENAME ofn;// Structure that contains attachment file info
@@ -17,9 +19,9 @@ HINSTANCE hInstance;
 //Function prototypes
 void generateViews(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void selectCommPort(HANDLE, LPCSTR, HWND, bool&);
-void connect(HANDLE&, LPCSTR, bool&);
-void disconnect(HANDLE&, bool&, LPCSTR);
+//void selectCommPort(HANDLE, LPCSTR, HWND, bool&);
+//void connect(HANDLE&, LPCSTR, bool&);
+//void disconnect(HANDLE&, bool&, LPCSTR);
 void createUIWindows(HWND);
 void attach(void);
 void availableCOM(HWND);
@@ -37,20 +39,24 @@ static const DWORD USERINPUT_TEXTBOX_START_X = 240;
 static const DWORD USERINPUT_TEXTBOX_START_Y = 10;
 static const DWORD READINPUT_TEXTBOX_START_X = 240;
 static const DWORD READINPUT_TEXTBOX_START_Y = 220;
+static const DWORD STATS_TEXTBOX_START_X = 20;
+static const DWORD STATS_TEXTBOX_START_Y = 160;
 static const DWORD BUTTON_WIDTH = 100;
 static const DWORD BUTTON_HEIGHT = 30;
 static const DWORD CONNECT_BUTTON_X = 50;
-static const DWORD CONNECT_BUTTON_Y = 150;
+static const DWORD CONNECT_BUTTON_Y = 20;
 static const DWORD DISCONNECT_BUTTON_X = 50;
-static const DWORD DISCONNECT_BUTTON_Y = 250;
+static const DWORD DISCONNECT_BUTTON_Y = 60;
 static const DWORD ATTACH_BUTTON_X = 50;
-static const DWORD ATTACH_BUTTON_Y = 350;
+static const DWORD ATTACH_BUTTON_Y = 100;
+
 
 
 HDC hdc;
 TEXTMETRIC tm;
 HWND userInputTextBox;
 HWND readInputTextBox;
+HWND statsTextBox;
 HWND hConnectButton;
 HWND hDisconnectButton;
-HWND browse;
+HWND browseButton;
