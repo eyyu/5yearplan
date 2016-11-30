1262d3f5dfa82b441944f1d45b047562736f9bd5
megapers <megapers@gmail.com>
2016-11-29 16:35:40 -0800

    TM: added new const integral values - idetifiers of controls and dialogs

--- 5yearplan/winmenu2.h
+++ 5yearplan/winmenu2.h
@@ -1,26 +1,57 @@
 #pragma once
-#define IDM_TX            100
-#define IDM_RX            101
-#define IDM_Port        102
-#define IDM_Speed        103
-#define IDM_Word        104
-#define IDM_ZETA        105
-#define IDM_Save        106
-#define IDM_THETA        107
-#define IDM_HELP        108
-#define IDM_NEWCONNECT    109
-#define IDM_PROPERTIES    110
-#define IDM_Settings        111
-#define IDM_COM4        112
-#define IDM_9600        113
-#define IDM_32000        114
-#define IDM_110000        115
-#define IDM_Connect        118
-#define IDM_Disconnect    119
-#define IDM_Exit        120
-#define IDM_COM_COMBOBOX    121
-#define IDM_OK            122
-#define IDM_CANCEL        123
-#define IDD_COMDIALOG    124
+//************Combo box********************
+#define IDM_COM_COMBOBOX	100
+#define IDM_OK				101
+#define IDM_CANCEL			102
+//************************************
+
+#define IDD_COMDIALOG		103
+#define IDD_DIALOG_MAIN		104
+
+//************Menu********************
+#define MENU_MYMENU			105
+#define MENU_NEW_CON		106
+#define MENU_PROP			107
+#define MENU_EXIT			108
+#define MENU_HELP			109
+//************************************
+
+//************Edit boxes*****************
+#define IDC_TX				110
+#define EDIT_TX				111//Editable textbox for TX input
+#define IDC_RX				112
+#define EDIT_RX				113//Read only textbox for RX output
+#define ATTACHMENT			134//holds name of the currently atttached file
+//************************************
+
+//************Buttons*****************
+#define BTN_CONNECT			114
+#define BTN_DISCONNECT		115
+#define BTN_ATTACH			116
+#define BTN_SEND			133
+//************************************
+
+//*******TX_STAT_GROUPBOX*************
+#define TX_STAT				117
+#define PACK_SENT			118 //Field to be populated with stats (# of packets sent)
+#define TX_STAT_SENT		119
+#define TX_STAT_ACK			120
+#define ACK_RECD			121//Field to be populated with stats (# of ACKs sent)
+#define TX_STAT_COMP		122
+#define TX_COMP				123
+//************************************
+
+//*******RX_STAT_GROUPBOX*************
+#define RX_STAT				124
+#define PACK_RECD			125 //Field to be populated with stats (# of packets received)
+#define RX_STAT_RECD		126
+#define RX_STAT_ACK			127
+#define ACK_SENT			128//Field to be populated with stats (# of ACKs received)
+#define RX_STAT_COMP		129
+#define RX_COMP				130
+//************************************
+
+#define HELP_COMDIALOG		131
+#define HELP_OK				132
 
 
