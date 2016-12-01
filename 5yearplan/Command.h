b91e1ae41754099178e0b094a2e438b0a0b6f47b
megapers <megapers@gmail.com>
2016-11-30 18:39:17 -0800

    TM: added C++ string for user input with dynamic size

--- 5yearplan/Command.h
+++ 5yearplan/Command.h
@@ -21,13 +21,16 @@
 #include <stdio.h>
 #include "winmenu2.h"
 #include "Connect.h"
+#include <string>
+
+using namespace std;
 
 LPCSTR	lpszCommName = "COM5";
 LPTSTR filePath;//String that holds path of the browsed file
 COMMCONFIG cc; // configuration struct for com port.
 char szFile[100];
-char message[500]; //String that holds user's input into the Tx EDITTEXT 
 char comPort[10];//Currently selected COMM PORT
+string text;//Holds user's input
 OPENFILENAME ofn;// Structure that contains attachment file info
 HDC hdc;
 HINSTANCE hInstance;
@@ -40,3 +43,5 @@ BOOL attach(void);
 INT_PTR CALLBACK comDialogProc(HWND, UINT, WPARAM, LPARAM);
 void configCommPort(HANDLE, LPCSTR, HWND);
 void availableCOM(HWND);
+void buttonEnable();
+void buttonDisable();
