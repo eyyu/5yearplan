4c5da467c33569b2b6f11a36715671da6aa9d69e
Eva <eva.yw.yu@gmail.com>
2016-12-04 12:30:56 -0800

    EY: final comments added for transmission header file

--- 5yearplan/transmission.h
+++ 5yearplan/transmission.h
@@ -5,6 +5,7 @@
 --
 -- FUNCTIONS:
 -- *list all functions here!* 
+-- Transmitter                         (void); // CTOR
 -- void        addDataToQueue          (const std}::string& );
 -- void        addFileToQueue          (const LPTSTR}& );
 -- void        addFileToQueue          (const std}::string& );
@@ -45,24 +46,26 @@
 namespace transmit {
     class Transmitter {
         static std::atomic_bool timeoutReached;
-        static void ackTimeout();
+        static void ackTimeout();       //timeout to receive an ACK for packet sent
 
-        std::queue<Packet> outputQueue;
-        int retryCounter = 0;
+        std::queue<Packet> outputQueue;  
+        int retryCounter = 0;           // dependent on MAX_RETRIES constant
 
         typedef Timer<&ackTimeout, (PACKET_SIZE / BAUD_RATE) * 1000> AckTimer;
-        AckTimer ackTimer;
+        AckTimer ackTimer; // Timer Object to mearure timeout 
+                           // for ACK after sending packet  
 
         std::vector<std::string> packetizeData(const std::string& data) const;
         Packet buildPacket(const std::string& data) const;
 
     public:
         Transmitter() = default;
-        void addDataToQueue(const std::string& data);
-        void addFileToQueue(const LPTSTR& filePath);
-        void addFileToQueue(const std::string& filePath);
-        void sendPacket(const HANDLE& commHandle);
-        bool outGoingDataInBuffer() const { return !outputQueue.empty(); };
-        void closeTransmitter();
+        void addDataToQueue(const std::string& data);     // data received as const a string 
+        void addFileToQueue(const LPTSTR& filePath);      // path to data is receives. Stream needed to read
+        void addFileToQueue(const std::string& filePath); // path to data is receives. Stream needed to read
+        void sendPacket(const HANDLE& commHandle);        // Called by connect.  
+        bool outGoingDataInBuffer() const { return !outputQueue.empty(); }; // allows for check of availability 
+                                                                            // of outgoing data 
+        void closeTransmitter(); 
     };
 }
