69aa613d6fce41a26c9e3c4446e9d89f6c30e340
Eva <eva.yw.yu@gmail.com>
2016-12-05 12:33:04 -0800

    EY: Commenting to Transmission.h

--- 5yearplan/transmission.h
+++ 5yearplan/transmission.h
@@ -1,3 +1,34 @@
+/*------------------------------------------------------------------------------
+-- SOURCE FILE: .cpp - The COMMAND or IDLE state of the protocol
+--
+-- PROGRAM: [5YearPlan] 
+--
+-- FUNCTIONS:
+-- *list all functions here!* 
+-- Transmitter                         (void); // CTOR
+-- void        addDataToQueue          (const std}::string& );
+-- void        addFileToQueue          (const LPTSTR}& );
+-- void        addFileToQueue          (const std}::string& );
+-- void        sendPacket              (const HANDLE& );
+-- bool        outGoingDataInBuffer    (void);
+-- void        closeTransmitter        (void);
+-- static void ackTimeout              (void);
+-- Packet      buildPacket             (const striag) const;
+--
+-- DATE: Nov. 09, 2016
+--
+-- REVISIONS: 
+-- Version 1.2.1.0 - [JA] - Nov. 09, 2016 - initial 
+-- Version 2.0.0.0 - [JA] - Nov. 09, 2016 - adding the "DC1" file start function
+--
+-- DESIGNER: Tim Makimov & John Agapeyev
+--
+-- PROGRAMMER: John Agapeyev
+--
+-- NOTES:
+-- This header holds the class declaration of the transmission object
+-- the Tx Object deals with outgoing packages  
+------------------------------------------------------------------------------*/
 #pragma once
 
 #include <string>
@@ -15,24 +46,26 @@
 namespace transmit {
     class Transmitter {
         static std::atomic_bool timeoutReached;
-        static void ackTimeout();
+        static void ackTimeout();       //timeout to receive an ACK for packet sent
 
-        std::queue<Packet> outputQueue;
-        int retryCounter = 0;
+        std::queue<Packet> outputQueue;  
+        int retryCounter = 0;           // dependent on MAX_RETRIES constant
 
         typedef Timer<&ackTimeout, (PACKET_SIZE / BAUD_RATE) * 1000> AckTimer;
-        AckTimer ackTimer;
+        AckTimer ackTimer; // Timer Object to mearure timeout 
+                           // for ACK after sending packet  
 
         std::vector<std::string> packetizeData(const std::string& data) const;
         Packet buildPacket(const std::string& data) const;
 
     public:
         Transmitter() = default;
-        void addDataToQueue(const std::string& data);
-        void addFileToQueue(const LPTSTR& filePath);
-        void addFileToQueue(const std::string& filePath);
-        void sendPacket(const HANDLE& commHandle);
-        bool outGoingDataInBuffer() const { return !outputQueue.empty(); };
-        void closeTransmitter();
+        void addDataToQueue(const std::string& data);     // data received as const a string 
+        void addFileToQueue(const LPTSTR& filePath);      // path to data is receives. Stream needed to read
+        void addFileToQueue(const std::string& filePath); // path to data is receives. Stream needed to read
+        void sendPacket(const HANDLE& commHandle);        // Called by connect.  
+        bool outGoingDataInBuffer() const { return !outputQueue.empty(); }; // allows for check of availability 
+                                                                            // of outgoing data 
+        void closeTransmitter(); 
     };
 }
