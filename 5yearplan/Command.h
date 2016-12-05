6ab9d425e6118c471b677577580f1ab3bfdb8830
Eva <eva.yw.yu@gmail.com>
2016-12-05 12:30:35 -0800

    EY: function prototype in header commenting for command.h

--- 5yearplan/Command.h
+++ 5yearplan/Command.h
@@ -1,14 +1,25 @@
 /*------------------------------------------------------------------------------
--- SOURCE FILE: main.cpp - The COMMAND state of the protocol
+-- SOURCE FILE: main.cpp - The COMMAND state of the protocol. 
+-- 							Main entrance point
 --
--- PROGRAM: [5YearPlan]
+-- PROGRAM: 5YearPlan
 --
 -- DATE: Nov. 29, 2016
 --
--- REVISIONS:
--- Version 2.0.
+-- FUNCTIONS:
+-- BOOL CALLBACK 	WndProc			(HWND, UINT, WPARAM, LPARAM);
+-- BOOL 			attach			(void);
+-- INT_PTR CALLBACK comDialogProc	(HWND, UINT, WPARAM, LPARAM);
+-- void 			configCommPort	(HANDLE, LPCSTR, HWND);
+-- void 			availableCOM	(HWND);
+-- void 			buttonEnable	(void);
+-- void 			buttonDisable	(void);
 --
--- DESIGNER: Tim Makimov
+-- REVISIONS:
+-- Version 1.0.1.0 - [TM] - 2016/NOV/19 - created functions
+-- Version 2.0.1.0 - [TM] - 2016/NOV/29 - updated version 
+--
+-- DESIGNER: Tim Makimov & Eva Yu
 --
 -- PROGRAMMER: Tim Makimov
 --
@@ -25,19 +36,27 @@
 
 using namespace std;
 
-LPCSTR	lpszCommName = "COM5";
-LPTSTR filePath;//String that holds path of the browsed file
-COMMCONFIG cc; // configuration struct for com port.
-char szFile[100];
-char comPort[10];//Currently selected COMM PORT
-string text;//Holds user's input
+/****************************
+DATA MEMEBER DECLARATIONS*
+******************************/
+LPCSTR	lpszCommName = "COM5"; // *** TO TM: IF NOT NEEDED, PLEASE REMOVE. 
+
+LPTSTR filePath; 	//String that holds path of the browsed file
+string text;		//Holds user's input
+
+char szFile[100];  // *** TO TM: PLEASE REMOVE IF NOT NEEDED!
+char comPort[10];	//Currently selected COMM PORT
+
+COMMCONFIG cc; 		// configuration struct for com port.
 OPENFILENAME ofn;// Structure that contains attachment file info
 HDC hdc;
 HINSTANCE hInstance;
 HWND hwnd1; // Window handler for the main window
 MSG Msg;//Holds message(s) from message queue
 
-//Function prototypes
+/****************************
+MEMEBER FUNCTION DECLARATIONS*
+******************************/
 BOOL CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 BOOL attach(void);
 INT_PTR CALLBACK comDialogProc(HWND, UINT, WPARAM, LPARAM);
